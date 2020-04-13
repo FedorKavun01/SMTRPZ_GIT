695ee5a9a0a2e4618336a7c4441b9f96eaa127c8
Unknown <omax1972@gmail.com>
2020-04-13 09:50:20 +0300

    All new

--- src/Lab6/Program.java
+++ src/Lab6/Program.java
@@ -2,24 +2,15 @@ package Lab6;
 
 public class Program {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
-        String encryptStr = encrypt("Just do it!", (char) 10);
-        System.out.println(encryptStr);
-        String decryptStr = decrypt(encryptStr, (char) 10);
-        System.out.println(decryptStr);
->>>>>>> Lab6
     }
 
     public static String encrypt(String str, char a) {
         StringBuilder sb = new StringBuilder();
         for (Character b : str.toCharArray()) {
-<<<<<<< HEAD
+
             char result = (char)(b + a);
-=======
-            char result = b + a > Character.MAX_VALUE ? Character.MAX_VALUE : (char)(b + a);
->>>>>>> Lab6
+
             sb.append(result);
         }
         return sb.toString();
@@ -28,11 +19,7 @@ public class Program {
     public static String decrypt(String str, char a) {
         StringBuilder sb = new StringBuilder();
         for (Character b : str.toCharArray()) {
-<<<<<<< HEAD
-            char result = (char)(b - a);
-=======
             char result = b - a < 0 ? (char) 0 : (char)(b - a);
->>>>>>> Lab6
             sb.append(result);
         }
         return sb.toString();
